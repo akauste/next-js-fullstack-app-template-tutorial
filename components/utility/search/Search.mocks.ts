ed801cc7833bda37c2b502824d3d45dae861bf00
Aku Kauste <aku.kauste@mediaviisi.fi>
2022-09-14 10:42:38 +0300

    feat: implement search component

---
+++ components/utility/search/Search.mocks.ts
@@ -0,0 +1,14 @@
+import { ISearch } from './Search';
+
+const base: ISearch = {
+  sampleTextProp: 'Hello world!',
+};
+
+// const alt: ISearch = {
+//   sampleTextProp: 'Hello universe!',
+// };
+
+export const mockSearchProps = {
+  base,
+  // alt,
+};
